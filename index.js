d2bf94559d71a3acb4ab4b66979245788016aff2
Dmytro Kiperman <8boyss@ukr.net>
2023-09-05 19:05:46 +0100

    First commit

--- index.js
+++ index.js
@@ -17,15 +17,23 @@
  */
 
 // Створюємо об'єкт Book
+let Book = {
+  title: "Загальна Книга",
+  author: "Анонім",
+  pages: 0,
+  read: function () {
+    console.log(`Ви читаєте "${this.title}" від ${this.author}`);
+  },
+};
 
 console.log("Завдання: 1 ==============================");
 
 // Виводимо в консоль Об'єкт: Book
-
+console.log(Book);
 // Виводимо в консоль прототип Об'єкту: Book
-
+console.log(Book.hasOwnProperty("title"));
 // Викликаємо функцію read об'єкту Book
-
+Book.read();
 // 2. Наслідування від базового об'єкту Book
 
 /*
@@ -38,14 +46,15 @@ console.log("Завдання: 1 ==============================");
  */
 
 // Створюємо об'єкт Novel, наслідуємо властивості і функції від об'єкта Book
-
+let Novel = Object.create(Book);
 // Додаємо властивість genre
-
+Novel.genre = "Новела";
 console.log("Завдання: 2 ==============================");
 
 // Виводимо в консоль Об'єкт: Novel
-
+console.log(Novel);
 // Виводимо в консоль прототип Об'єкту: Novel
+console.log(Object.getPrototypeOf(Novel));
 
 // 3. Створення нового об'єкту та зміна його прототипу
 
@@ -61,14 +70,18 @@ console.log("Завдання: 2 ==============================");
  */
 
 // Створюємо об'єкт Biography
-
+let Biography = {
+  title: "Загальна Біографія",
+  author: "Біограф",
+  pages: 200,
+};
 // Змінемо прототип об'єкта Biography на Novel
-
+Object.setPrototypeOf(Biography, Novel);
 console.log("Завдання: 3 ==============================");
 // Виводимо в консоль Об'єкт: Biography
-
+console.log(Biography);
 // Перевіримо чи являється Novel прототипом Biography та виведемо в консоль
-
+console.log(Novel.isPrototypeOf(Biography));
 // 4. Інкапсуляція властивості та додання властивості
 /*
  * Об'єкт: ScienceBook
@@ -77,15 +90,25 @@ console.log("Завдання: 3 ==============================");
  */
 
 // Створюємо ScienceBook, наслідуємо властивості і функції від об'єкта Book
-
+let ScienceBook = Object.create(Book);
 // Додаємо властивість 'info' за допомогою Object.defineProperty
-// Зробимо щоб 'info' не можно було видалити або змінити, перевіримо і спробуємо присвоїти ій будь яке значення (це потрібно робити ззовні defineProperty),
-// Отримаємо помилку Cannot assign to read only property 'info' of object '#<Object>'
+Object.defineProperty(ScienceBook, "info", {
+  configurable: false,
 
-// Далі створюємо сетер який присвоє властивості info значення яке отримує при виклику, помилку більше не отримуємо але при спробі вивести значення info отримуємо undefined
+  // Зробимо щоб 'info' не можно було видалити або змінити, перевіримо і спробуємо присвоїти ій будь яке значення (це потрібно робити ззовні defineProperty),
+  // Отримаємо помилку Cannot assign to read only property 'info' of object '#<Object>'
 
-// Створимо гетер який буде нам повертати рядок: Про книгу <title>: <info>
-// тепер все виводить коректно
+  // Далі створюємо сетер який присвоє властивості info значення яке отримує при виклику, помилку більше не отримуємо але при спробі вивести значення info отримуємо undefined
+  set(value) {
+    this._info = value;
+  },
+
+  // Створимо гетер який буде нам повертати рядок: Про книгу <title>: <info>
+  // тепер все виводить коректно
+  get() {
+    return `Про книгу ${this.title}: ${this._info}`;
+  },
+});
 
 // Заповнюємо об'єкт
 // | Властивість | Значення             |
@@ -94,11 +117,15 @@ console.log("Завдання: 3 ==============================");
 // | author      | "Альберт Ейнштейн"   |
 // | info        | написана в 1915 році |
 
+ScienceBook.title = "Фізика 101";
+ScienceBook.author = "Альберт Ейнштейн";
+ScienceBook.info = "написана в 1915 році";
+
 console.log("Завдання: 4 ==============================");
 // Виводимо в консоль властивість info
-
+console.log(ScienceBook.info);
 // Виводимо в консоль налаштування властивости info
-
+console.log(Object.getOwnPropertyDescriptor(ScienceBook, "info"));
 // 5. Поліморфізм: створення нового об'єкта та перевизначення його методу
 /*
  * Об'єкт: Textbook
